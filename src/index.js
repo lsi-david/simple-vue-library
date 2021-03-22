5038d9b28333732577b80e57b9034291c5f14e29
lsi-david <david@lifesafetyint.com>
2021-03-22 13:51:09 -0600

    changes in main.js

--- src/index.js
+++
@@ -1,2 +0,0 @@
-export {default as SocialButton} from './components/SocialButton'
-export {default as FileInput} from './components/FileInput'
--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import * as components from './components'
+import * as components from './components/';
 
 const ComponentLibrary = {
   install(Vue, options = {}) {
     // components
     for (const componentName in components) {
-      const component = components[componentName]
+      const component = components[componentName];
 
-      Vue.component(component.name, component)
+      Vue.component(component.name, component);
     }
-  }
-}
+  },
+};
 
-export default ComponentLibrary
+export default ComponentLibrary;
 
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(ComponentLibrary)
+  window.Vue.use(ComponentLibrary);
 }
